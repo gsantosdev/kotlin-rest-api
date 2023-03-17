945652dae1a2a59bfe0c04b68ca45e7fcb59e187
santo <santosgustavo431@gmail.com>
2023-03-17 15:07:45 -0300

    Adding Message as example of data class - Changing controller to return a list of Message

---
+++ src/main/kotlin/app/model/Message.kt
@@ -0,0 +1,3 @@
+package app.model
+
+data class Message(val id: String?, val text: String)
--- src/main/kotlin/app/resource/MessageController.kt
+++ src/main/kotlin/app/resource/MessageController.kt
@@ -1,5 +1,6 @@
 package app.resource
 
+import app.model.Message
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -7,5 +8,9 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class MessageController {
     @GetMapping("/")
-    fun index(@RequestParam("name") name: String) = "Hello, $name!"
+    fun index() = listOf(
+            Message("1", "Hello!"),
+            Message("2", "Bonjour!"),
+            Message("3", "Privet!"),
+    )
 }
